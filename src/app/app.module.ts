a991be7f96ecabef8f22478fa0568bd88a4553e4
Hải Trình Nguyễn <nguyenmaster3012@gmail.com>
2023-10-12 03:02:33 +0700

    done ui order page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './header/header.component';
+import { DetailProductComponent } from './detail-product/detail-product.component';
 import { FooterComponent } from './footer/footer.component';
+import { OrderComponent } from './order/order.component';
 
 @NgModule({
-  declarations: [HomeComponent, HeaderComponent, FooterComponent],
+  declarations: [
+    HomeComponent,
+    DetailProductComponent,
+    HeaderComponent,
+    FooterComponent,
+    OrderComponent,
+  ],
   imports: [BrowserModule],
   providers: [],
-  bootstrap: [HomeComponent],
+  bootstrap: [
+    // HomeComponent,
+    //DetailProductComponent,
+    OrderComponent,
+  ],
 })
 export class AppModule {}
