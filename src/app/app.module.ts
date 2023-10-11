9c6bc191f57e135dcda8bb6238ea1a0b7ec6b381
Hải Trình Nguyễn <nguyenmaster3012@gmail.com>
2023-10-12 00:38:04 +0700

    setup project angular basic

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [HomeComponent],
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [HomeComponent],
 })
-export class AppModule { }
+export class AppModule {}
