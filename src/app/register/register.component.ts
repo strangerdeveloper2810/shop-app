5a08d7677cfc431442d1ab4ec907fd72c005fb57
Trình Nguyễn <nguyenmaster3012@gmail.com>
2024-09-26 02:50:41 +0700

    validate form register with angular form and directive ngIf

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
-
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss'],
 })
 export class RegisterComponent {
+  @ViewChild('registerForm') registerForm!: NgForm;
   // Khai báo các biến tương ứng với các trường dữ liệu trong form
   phone: string = '';
   password: string = '';
@@ -39,5 +40,42 @@ export class RegisterComponent {
       `fullName: ${this.fullName}` +
       `isAccepted: ${this.isAccepted}`;
     alert(message);
+  }
+
+  //check password match
+  checkPasswordMatch() {
+    console.log('aaaaa');
+    if (this.password !== this.retypePassword) {
+      this.registerForm.form.controls['retypePassword'].setErrors({
+        passwordMismatch: true,
+      });
+    } else {
+      this.registerForm.form.controls['retypePassword'].setErrors(null);
+    }
+  }
+
+  //check age
+  checkAge() {
+    if (this.dateOfBirth) {
+      const today = new Date();
+      const birthDate = new Date(this.dateOfBirth);
+      let age = today.getFullYear() - birthDate.getFullYear();
+      const monthDiff = today.getMonth() - birthDate.getMonth();
+
+      if (
+        monthDiff < 0 ||
+        (monthDiff === 0 && today.getDate() < birthDate.getDate())
+      ) {
+        age--;
+      }
+
+      if (age < 18) {
+        this.registerForm.form.controls['dateOfBirth'].setErrors({
+          invalidAge: true,
+        });
+      } else {
+        this.registerForm.form.controls['dateOfBirth'].setErrors(null);
+      }
+    }
   }
 }
